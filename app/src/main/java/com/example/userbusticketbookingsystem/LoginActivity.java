7f2b45f93f0bd014eeb5c26f798940e3db5d6bbf
aminul-islam6911 <aikayoum420@gmail.com>
2022-07-25 06:37:25 +0600

    complete

--- app/src/main/java/com/example/userbusticketbookingsystem/LoginActivity.java
+++ app/src/main/java/com/example/userbusticketbookingsystem/LoginActivity.java
@@ -102,7 +102,7 @@ public class LoginActivity extends AppCompatActivity {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        LoginActivity.super.onBackPressed();
+                        finishAffinity();
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
